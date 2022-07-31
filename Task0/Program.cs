5c252bf19eae95504f3f19526ad2b67305d206b1
Ilfat <78148851+Il777-hash@users.noreply.github.com>
2022-07-31 23:34:22 +0500

    Добавьте файлы проекта.

---
+++ Task0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Task0.Models;
+
+namespace Task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using ShopDBContext db = new ShopDBContext();
+            {
+                foreach (Product product in db.Products)
+                {
+                    Console.WriteLine($"Описание: {product.Description} Цена: {product.UnitPrice} Вес: {product.Weight}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
